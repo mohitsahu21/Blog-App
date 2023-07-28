6029be1292337e270b406ae7bf4f19bc9dde7329
mohitsahu21 <mohitsahu1993@gmail.com>
2023-07-28 19:38:16 +0530

    navbar done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import Logo from "../images/logo.png"
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='container'>
+            <div className="logo">
+                <img src={Logo} alt='logo'/>
+            </div>
+            <div className="links"> 
+                <Link to="/?cat=art" className='link'> <h6>ART</h6></Link>
+                <Link to="/?cat=science" className='link'> <h6>SCIENCE</h6></Link>
+                <Link to="/?cat=technology" className='link'> <h6>TECHNOLOGY</h6></Link>
+                <Link to="/?cat=cinema" className='link'> <h6>CINEMA</h6></Link>
+                <Link to="/?cat=design" className='link'> <h6>DESIGN</h6></Link>
+                <Link to="/?cat=food" className='link'> <h6>FOOD</h6></Link>
+                <span>username</span>
+                <span>logout</span>
+                <span className='write'><Link to="write" className='link'>Write</Link></span>
+                
+                
+            </div>
+
+        </div>
+    </div>
   )
 }
 
